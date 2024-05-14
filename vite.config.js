5216b7dd1a0fea8e21193fedae77065952d1794e
ShomaTakami <hawklook.2331@gmail.com>
2024-05-14 18:12:54 +0900

    bbb

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,6 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
-            buildDirectory: 'dist',
         }),
     ],
-    build: {
-        outDir: 'dist',
-    },
 });
