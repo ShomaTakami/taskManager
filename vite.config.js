f7355e7c91bba4559ad0d5ab2d27c8f0acf75977
ShomaTakami <hawklook.2331@gmail.com>
2024-05-14 17:47:16 +0900

    tewst

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,10 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+            buildDirectory: 'dist',
         }),
     ],
+    build: {
+        outDir: 'dist',
+    },
 });
